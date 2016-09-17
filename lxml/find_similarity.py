f3502a023faa7c76b7fbb89314566dead6e906c0
Juhi Ghosh <juhi.ghosh@students.iiit.ac.in>
2016-09-17 21:47:12 +0530

    Parsing query, generating synonyms, finding similarity

--- lxml/find_similarity.py
+++ lxml/find_similarity.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 from nltk.corpus import wordnet as wn
 
-synset_keys = {'price' : wn.synsets('price')[0],
-					 'food' : wn.synsets('food')[0],
-					 'ambience' : wn.synsets('ambience')[0],
-					 'service' : wn.synsets('service')[0]}
 
+categories = {'places' : wn.synsets('places')[0],
+	      'details' : wn.synsets('details')[0],
+              'ratings' : wn.synsets('ratings')[0],
+              'reviews' : wn.synsets('reviews')[0]}
 
-# find out the similarity score between terms from query and the keys in our data store of states
-def detect_similarity(term):
+#find out the similarity score between terms from query and the keys in our data store of states
+def get_similarity_score(term):
 	try:
 		wn_term = wn.synsets(term)[0]
 	except:
+		#print term
 		return
-
 	max_score = -100
 	max_sim_key = ''
-	for cat, value in synset_keys.iteritems():
+	for cat, value in categories.iteritems():
 		sim_score = wn.wup_similarity(wn_term, value)
 		#print cat, term, sim_score
 		if sim_score > max_score:
--- lxml/gen_synsets.py
+++ lxml/gen_synsets.py
@@ -1,26 +1,33 @@
-qry_categories = dict()
+from find_similarity import get_similarity_score
 from nltk.corpus import wordnet as wn
 
+qry_terms = dict()
+
+#want to travel from Mumbai to Mount Abu guide me
 query="tell me the details about places to visit in Rajasthan"
-#pos tag query, to extract relevant terms, hence prepare below list
+#use pos tagged list, prepared in parse_query.py file
 
 list_of_terms = query.split(" ");
 
-# append synsets
+#append synsets for terms
 for term in list_of_terms:
 	for i in wn.synsets(term):
 		key=wn.morphy(term)
                 for j in i.lemmas():  # Iterating through lemmas for each synset.
 			# append synonyms
 			syn = str(j.name())
-			if key in qry_categories:
-    				qry_categories[key].append(syn)
+			if key in qry_terms:
+    				qry_terms[key].append(syn)
 			else:
         			# create a new array in this slot
-        			qry_categories[key] = [syn]
-			
+        			qry_terms[key] = [syn]
 
-for c in qry_categories:
-	qry_categories[c] = list(set(qry_categories[c]))
-	#print c, ':', qry_categories[c]
-print qry_categories
+for c in qry_terms:
+	qry_terms[c] = list(set(qry_terms[c]))
+	#print c, ':', qry_terms[c]
+print qry_terms
+
+for cat, val_lst in qry_terms.iteritems():
+	for entry in val_lst:
+		print get_similarity_score(entry)
+
